eb4f7f9abe51b9175ff193875657e0976273d8ae
sahutt <stefaniehuttelmaier2024@u.northwestern.edu>
2024-07-07 14:54:31 -0500

    Completed lab

--- Huttelmaier_Stefanie_L03.R
+++ Huttelmaier_Stefanie_L03.R
@@ -196,3 +196,98 @@ people2 <- tribble(
 
 people2 |> pivot_wider(names_from = key,
                       values_from = value)
+
+
+# Case Study --------------------------------------------------------------
+
+# Tinder is interested in knowing more about their most active users and have 
+# tasked you with exploring their 7 most active users during 2020. The dataset 
+# containing the top 7 active users during 2020 is stored in the `data` folder 
+# and called `users_top7_2020.csv`.^[This dataset was sourced from 
+# [Swipestats.io](https://www.swipestats.io/).]
+
+# The column names contain prefixes "matches", "likes", and "passes" followed 
+# by a number; the suffix number represents the month; and the cell represents 
+# either the total number of matches (matches), total number of times the user 
+# swiped right (likes), or total number of times the user swiped left (passes) 
+# during that month (ie: `matches_1` = total number of matches during January 
+# 2020).
+
+#Complete the following tasks:
+  
+# a)  Use an appropriate readr function to read in `users_top7_2020.csv`
+
+# b)  Is `users_top7_2020.csv` a `tibble` and how do you know? If it isn't, 
+# then figure how to turn it into a `tibble`.
+
+# c) `Use an appropriate graph to visualize the matches, likes, and passes 
+# over time for each user. What insights and conclusions can you gain from 
+# this graph, if any.
+
+# d)  Write out a copy of the tidy dataset to the `data` sub-directory as 
+# an RDS file named `users_tidy.rds`. 
+
+# e) What is one benefit of saving a dataset as an `rds` compared to a `csv`?
+
+# read in using readr
+top_user_2020 <- read_csv("data/users_top7_2020.csv")
+
+#check to make sure it read in as a tibble
+is_tibble(top_user_2020)
+
+# tidy the data
+users_tidy_1 <- top_user_2020 |> 
+  #pivot everything but the user id column
+  pivot_longer(cols = !user_id,
+               #need to split each column into two, the value (match, passes or
+               # likes) and the month)
+               names_to = c(".value", "month"),
+               #separator in the column head is an underscore
+               names_sep = "_",
+               #value from the existing cell goes into a new column "count"
+               values_to = "count")
+
+# still not tidy enough to fit onto one plot
+users_tidy_1 |> 
+  ggplot(aes(x = month, y = matches, group = user_id)) +
+  geom_line(alpha = 0.25) +
+  scale_y_reverse()
+
+#take 2 at tidying
+#something still wonky, I think with month as character
+users_tidy_2 <- top_user_2020 |> 
+  pivot_longer(cols = !user_id,
+               names_to = c("category", "month"),
+               names_sep = "_",
+               values_to = "count") 
+
+users_tidy_2 |> 
+  ggplot(aes(x = month, y = count, color = category)) +
+  geom_line() +
+  facet_wrap(~user_id)
+
+#take 3 tidying and visualizing
+#parse number leaves the number in matches
+top_user_2020 |> 
+  pivot_longer(cols = !user_id,
+               names_to = "category",
+               values_to = "count") |> 
+  mutate(month = parse_number(category))
+
+users_tidy <- top_user_2020 |> 
+  pivot_longer(cols = !user_id,
+               names_to = c("category", "month"),
+               names_sep = "_",
+               values_to = "count")  |> 
+  mutate(month = as.numeric(month))
+
+write_rds(users_tidy, file = "data/users_tidy.rds")
+
+users_tidy |> 
+  ggplot(aes(x = month, y = count, color = category)) +
+  geom_line() +
+  facet_wrap(~user_id, scales = "free_y") +
+  scale_x_continuous(name ="Month in 2020", 
+                  breaks=c(2, 4, 6, 8, 10, 12)) +
+  labs(y = "Count",
+       title = "Top 7 user behavior by month in 2020")
